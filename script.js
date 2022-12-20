f145ea959a9e82ae1f467bd2f9fedbdf34502aa6
Tropicaljittai <93302274+Tropicaljittai@users.noreply.github.com>
2022-12-20 10:58:08 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
-// Add your answer here
+$('#htmlcss').hover(function() {
+    $('#htmldesc').addClass('text-active');
+  },
+  function() {
+    $('#htmldesc').removeClass('text-active');
+  });   
+
+  $('#jQuery').hover(function() {
+    $('#jQuerydesc').addClass('text-active');
+  },
+  function() {
+    $('#jQuerydesc').removeClass('text-active');
+  });   
+
+  $('#ruby').hover(function() {
+    $('#rubydesc').addClass('text-active');
+  },
+  function() {
+    $('#rubydesc').removeClass('text-active');
+  });   
+
+  $('#php').hover(function() {
+    $('#phpdesc').addClass('text-active');
+  },
+  function() {
+    $('#phpdesc').removeClass('text-active');
+  });   
+
